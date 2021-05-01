b7a4e3ec13e07abfbebf548904b9168868f3296e
xprmnts <abhinay@outlook.com>
2021-05-01 09:25:10 -0400

    added header and header styles

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import classes from './Header.module.css';
+
+const Header = () => {
+    return (
+        <header className={classes.header}>
+            <h1>Abhinay Reddy</h1>
+            <h2>Software Engineer</h2>
+        </header>
+    );
+};
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import Header from '../components/Header';
+
 export default function Home() {
     return (
         <div>
-            <h1>Abhinay Reddy</h1>
+            <Header />
         </div>
     );
 }
