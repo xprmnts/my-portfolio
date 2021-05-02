a595dba4367529c7f439920c6637d85b9bbd8d19
xprmnts <abhinay@outlook.com>
2021-05-02 14:32:15 -0400

    added head tag with updated meta, title and analytics tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import { Fragment } from 'react';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <Fragment>
+            <Head>
+                <meta charSet='utf-8' />
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1'
+                />
+                <title>Abhinay Reddy</title>
+                <meta name='author' content='Abhinay Reddy' />
+                <link rel='icon' type='image/svg' href='/arlogo.svg' />
+                {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+                <script
+                    async
+                    src='https://www.googletagmanager.com/gtag/js?id=G-YYLF8TN14S'
+                ></script>
+            </Head>
+            <Component {...pageProps} />
+        </Fragment>
+    );
 }
 
-export default MyApp
+export default MyApp;
